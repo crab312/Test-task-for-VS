454c9146c07dd201862588343ee8bfd55ea93992
Vladimir Salnikov <reg312@gmail.com>
2016-06-20 13:55:10 +0300

    few more tests

--- TestTaskForVS/UnitTestProject1/UnitTest1.cs
+++ TestTaskForVS/UnitTestProject1/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestNormalExpressions()
         {
             /* Simple expressions */
             TestNumExpression("1 + 2 = 0", 3);
@@ -18,6 +18,8 @@ namespace UnitTestProject1
             TestNumExpression("-1 - 2 = 0", -3);
             TestNumExpression("-5000 + 1900 + 2100 = 0", -1000);
             TestNumExpression("1-2-3 = 0", -4);
+            TestNumExpression("-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1=2+2+2+2+2+2+2+2+2+2+2+2", -49);
+
 
             /* Some bracketed expressions */
             TestNumExpression("-(1-1-1) = 0", 1);
@@ -39,6 +41,44 @@ namespace UnitTestProject1
             /* Test polynom standartization */
             CheckPolynom("x + 2.1x^2 = 100x - 4x^2", "6.1x^2 - 99x = 0");
             CheckPolynom("10 - x^3 + 8x^4 = 10x - 1 + 5x^3", "8x^4 - 6x^3 - 10x + 11 = 0");
+        }
+
+
+        [TestMethod]
+        public void TestBracketedExpressions()
+        {
+            CheckPolynom("(x+y)(x-y)=0", "x^2 - y^2 = 0");
+            CheckPolynom("(x+y)(x+y)=0", "x^2 + y^2 + 2xy = 0");
+            CheckPolynom("(x-y)(x-y)=0", "x^2 + y^2 - 2xy = 0");
+
+            TestExpressionXY("(x+y)(x+y)(x+y)=0", 2, 3, 125);
+            TestExpressionXY("(x+y)(x-y)(x+y)(x-y)(x+y)(x-y)(x+y)(x-y)(x+y)(x-y)(x+y)(x-y)=0", 2,3, 15625);
+        }
+
+        [TestMethod]
+        public void TestWeirdExpressions()
+        {
+            /* Test syntax stuff */
+            CheckPolynom("- x =     + x ", "-2x=0");
+            
+            /* Test unexpected stuff */
+            CheckPolynom("0x = 0", "0=0");
+            CheckPolynom("0 = x1", "-x=0");
+            CheckPolynom("x12 = 0", "12x=0");
+
+            CheckPolynom("0=2x^2+4y^100 - 0x", "-4y^100-2x^2=0");
+
+            /* Test errors handling */
+            TestIncorrectExpression("==");
+
+        }
+
+        [TestMethod]
+        public void TestWeirdVariableName()
+        {
+            CheckPolynom("xxx=-xxx", "2x^3=0");
+
+            TestIncorrectExpression("X=0");
         }
 
         [TestMethod]
@@ -71,6 +111,20 @@ namespace UnitTestProject1
             res = res.Replace(" ", "");
             expectedResult = expectedResult.Replace(" ","");
             Assert.AreEqual(expectedResult, res);
+        }
+
+        [TestMethod]
+        public void TestIncorrectExpression(string expr)
+        {
+            try
+            {
+                var exprObj = new Expression(expr);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
         }
     }
 }
